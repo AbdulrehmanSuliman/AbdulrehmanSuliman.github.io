8ae86a872ea503a174a814e26364ef8332ea5b91
AbdulrehmanSuliman <abdulrehman.kassem00@eng-std.cu.edu.eg>
2023-06-11 21:13:13 +0300

    title color

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="#" className="flex items-center">
         <img src="/Logo.svg" className="h-8 mr-3" alt="Voxight Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap">Voxight</span>
+        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Voxight</span>
     </a>
     
   </div>
