7ccbd2f7b50e3f024ecd858b909614ebddde45a6
AbdulrehmanSuliman <abdulrehman.kassem00@eng-std.cu.edu.eg>
2023-06-12 00:27:11 +0300

    adding google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '@/styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=G-85EYN9160Q`}
+      strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-85EYN9160Q');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
